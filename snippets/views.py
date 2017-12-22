f4d6b2b140fbe61e0e404c7506ed21ef5e8e995d
WinstonKamau <crispus.kamau@andela.com>
2017-12-22 13:07:25 +0300

    feat(snippet-view): add snippet views > > Add a snippet view to be used > Add a permision for only the created user to be able to perform put or delete functions > [Tutorial django ]

--- snippets/views.py
+++ snippets/views.py
@@ -1,15 +1,19 @@
 # Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
 from django.contrib.auth.models import User
 from rest_framework import generics
 from snippets.models import Snippet
 from rest_framework import permissions
-from snippets.serializers import SnippetSerializer
-from snippets.models import UserSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
 class SnippetList(generics.ListCreateAPIView):
     '''
     List all snippets or create a new snippet
     '''
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
@@ -20,7 +24,7 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     '''
     Retrieve, update or delete a snippet instance.
     '''
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
@@ -31,3 +35,18 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
